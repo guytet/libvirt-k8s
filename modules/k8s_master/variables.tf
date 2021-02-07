f9879a2d46c7cb74e554e24b40659eb3b7c38218
guytet <none@none.com>
2021-02-06 22:55:49 -0500

    WIP

--- modules/k8s_master/variables.tf
+++ modules/k8s_master/variables.tf
@@ -1,6 +1,6 @@
 
 variable "master_name" {
-  default = ""
+  default = "k8s-master"
 }
 
 variable "user_name" {
@@ -15,27 +15,27 @@ variable "ssh_keys" {
 }
 
 variable "ram" {
-  default = ""
+  default = "2048"
 }
 
 variable "vcpu" {
-  default = ""
+  default = "2"
 }
 
 variable "autostart" {
-  description = "Whether VM should autostart on hosts powerup"
+  description = "Whether the VM should autostart when libvirt starts"
   type    = bool
   default = false
 }
 
 
 variable "bridge_name" {
-  description = "assuming bridge as the preferred networking model for you VM, name of host's bridge to bind to"
+  description = "assuming bridge as the preferred networking model for your VM, name of the host's bridge to bind to"
   default = "br0"
 }
 
 variable "qemu_image_path" {
-  description = "path or URl for qemu image"
+  description = "path or URL for qemu image"
   default = "http://cloud-images.ubuntu.com/releases/bionic/release-20191008/ubuntu-18.04-server-cloudimg-amd64.img"
 }
 

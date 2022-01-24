b2e4e7dae2b09eb75ead13bb9b32c38148c7e2f7
guytet <none@none.com>
2022-01-23 22:15:40 -0500

    WIP

--- helm/consul/helm_apply.sh
+++ helm/consul/helm_apply.sh
@@ -1,5 +1,1 @@
-helm upgrade  --install \
-consul hashicorp/consul \
---create-namespace \
- --namespace consul \
---set server.storageClass=local-storage
+helm upgrade  --install consul hashicorp/consul --create-namespace -n consul --set server.storageClass=local-storage --set client.enabled=false --set server.replicas=2
